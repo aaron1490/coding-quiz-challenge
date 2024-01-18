ed9594c91492aae29b8fa139e29bcd037b3b54c1
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-01-18 18:31:23 +0000

    bug: fixed issue with questions jumping from question 1 stright to 5, not it only jumps from 1 to 3

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,6 +8,7 @@ var questionTitleEl = document.getElementById('question-title')
 var choicesEl = document.getElementById("choices");
 var timerEl = document.getElementById("time");
 var feedbackEl = document.getElementById("feedback");
+var currentQuestionNumber;
 
 var startBtn = document.getElementById('start');
 var submitEl = document.getElementById("submit");
@@ -58,8 +59,8 @@ function currentQuestion() {
   var choices = questions[currentQuestionIndex].choices;
 
   // Display the question text
-  currentQuestion = questionIndex + 1;
-  questionTitleEl.innerText = `${currentQuestion}.) ${question.questionText}`;
+  currentQuestionNumber = currentQuestionIndex + 1;
+  questionTitleEl.innerText = `${currentQuestionNumber}.) ${question.questionText}`;
 
   // Display the choices
   choices.forEach(function(choice) {
@@ -68,12 +69,16 @@ function currentQuestion() {
     choiceBtn.classList.add("btn");
     choicesEl.appendChild(choiceBtn);
 
-    // add event listener to each choice
-    choicesEl.addEventListener("click", function(event) {
+  })
+
+
+}
+
+// add event listener to each choice
+    choicesEl.addEventListener("click", function (event) {
     checkAnswer(event);
     });
-  })
-}
+    
 function checkAnswer(event) {
   var selectedChoice = event.target.textContent; // Use event.target.textContent to get the selected choice
   var correctAnswer = questions[currentQuestionIndex].correctAnswer; // declaring correctAnswer variable to the correct answer for the quiz
@@ -83,24 +88,27 @@ function checkAnswer(event) {
   } else {
     wrongAnswer();
   }
-  currentQuestionIndex++;
-  if (currentQuestionIndex < questions.length) {
-    currentQuestion();
-  } else {
-    endQuiz();
-  }
+  // currentQuestionIndex++;
+  // if (currentQuestionIndex < questions.length) {
+  //   currentQuestion();
+  // } else {
+  //   endQuiz();
+  // }
 }
-
 
 
 function timeStart() {
   timerEl.textContent = timeLeft;
-  setInterval(function() {
-    timeLeft--;
-    timerEl.textContent = timeLeft;
-    // add timer that moves at intervial of -1000ms
+  var interval = setInterval(function () {
+    if (timeLeft > 0) {
+      timeLeft--;
+      timerEl.textContent = timeLeft;
+    } else {
+      clearInterval(interval);
+      endQuiz();
+    }
   }, 1000);
-};
+}
 
 
 function nextQuestion() {
@@ -138,108 +146,7 @@ function rightAnswer() {
 
 // Adding event listeners
 startBtn.addEventListener('click', startQuiz); // event listener for start button on quiz
-// choicesEl.addEventListener('click', rightAnswer, wrongAnswer); // event listener for answer buttons
+choicesEl.addEventListener('click', function (event) {
+  checkAnswer(event)
+}); // event listener for answer buttons
 // submitEl.addEventListener('click', submitScore); // event listener for submit button
-
-
-
-
-
-
-
-
-
-// Function for starting the timer countdown from 60 seconds when the above is clicked
-
-// Function for adding title of the question to the top
-// Function for replacing the button text with the questions from the array
-
-// Function for removing time if a wrong answer selected AND moving to next question
-// Function for moving to next question if right answer is selected
-
-// End screen gets unhidden and it gets displayed asking for user to input initials to log high score
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Element selectors
-
-// var questionsScreenEl = document.getElementById("questions");
-// var startScrenEl = document.getElementById("start-screen");
-// var choicesEl = document.getElementById("choices");
-
-// var timerEl = document.getElementById("time");
-// var scoreEl = document.getElementsByClassName("scores");
-// var endScreenEl = document.getElementById("end-screen");
-
-
-
-// // Declaring variables
-// var questionIndex = 0;
-// var score = 0;
-// var timeLeft = 60;
-// var currentQuestionIndex = 0;
-
-// // Declaring Constants
-// const penalty = 10
-
-
-// // Display quiz screen hiding landing page
-// startBtnEl.addEventListener("click", function () {
-//   var question = questions[0];
-//   questionIndex = 0;
-//   score = 0;
-//   questionTitleEl.innerText = questionIndex + 1 + ".) " + question.questionText;
-//   questionsScreenEl.classList.remove("hide"); // show the questions screen
-//   startScreenEl.classList.add("hide"); // hide the start screen
-//   nextQuestion();
-//   currentQuestion();
-//   // start timer function
-//   timeStart();
-//   wrongAnswer();
-//   rightAnswer();
-// });
-
-
-
-
-
-// function currentQuestion() {
-
-// }
-
-
-
-// // Display quiz title
-
-// // Display quiz choices
-
-// // Display timer with countdown
-
-// // Timer penalty
-
-// // set what happens when timer reaches 0
-
-// // Submit hi-score to highscore screen
